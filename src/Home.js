2ec7783aa44ca8b3bcfff81e27b42e85f7ee22f6
Sarila <ngakhusisarila@gmail.com>
2022-04-20 18:57:17 +0545

    more use of state hook and conditional loading

--- src/Home.js
+++ src/Home.js
@@ -5,18 +5,35 @@ const Home = () => {
     const title = "All BLogs";
 
    const [blogs, setBlogs] = useState(null);
+   const [isPending, setIsPending] = useState(true);
+   const [error, setError] = useState(null);
 
    useEffect(() => {
-       fetch('http://localhost:8000/blogs')
-       .then(res => {
-           return res.json();
-       })
-       .then((data) => {
-           setBlogs(data);
-       })
+       setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            if(!res.ok) {
+                throw Error('Could not fetch data');
+            }
+            return res.json();
+
+        })
+        .then((data) => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message);
+            // console.log(err.message);
+        })
+       }, 1000)
    }, []);
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            { isPending && <div>Loading.....</div>}
             {blogs && <Bloglist blogs= {blogs} title= {title} />}
         </div>
     );
