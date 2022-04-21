ecea89444168740c2d5acef6e708dc7f20a14e0f
Sarila <ngakhusisarila@gmail.com>
2022-04-21 17:47:09 +0545

    History hook for redirects

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = ()=> {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Sarila');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleEvent = (e) => {
+        e.preventDefault();
+        const blog ={title, body, author};
+
+        setIsPending(true);
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(blog)
+        }). then (() => {
+            console.log("new blog added");
+            setIsPending(false);
+            history.push('/');
+        })
+        
+    }
     return (
         <div className="create">
             <h1>Add a new Blog</h1>
+            <form onSubmit={handleEvent}>
+                <label>Blog Title:</label>
+                <input type="text" required value={title} onChange={(e)=> {setTitle(e.target.value)}}/>
+
+                <label>Blog Body:</label>
+                <textarea required value={body} onChange={(e)=>setBody(e.target.value)}></textarea>
+
+                <label>Blog Author:</label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="Sarila">Sarila</option>
+                    <option value="Mark">Mark</option>
+                    <option value="Thomas">Thomas</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog</button>}
+                
+            </form>
         </div>
     );
 } 
