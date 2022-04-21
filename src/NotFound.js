503faa4c887245939fe62f09760a93447ee42752
Sarila <ngakhusisarila@gmail.com>
2022-04-21 17:58:55 +0545

    404 pages added

---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return ( 
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>The page you are trying to visiot is not found</p>
+            <Link to="/">Back to the Home Page......</Link>
+        </div>
+     );
+}
+ 
+export default NotFound;
